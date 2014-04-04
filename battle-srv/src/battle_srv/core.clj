6403ce66e1dca3128b4e2484c2c8d3ca345adb21
Joakim Verona <joakim@verona.se>
2014-04-04 15:11:45 +0200

    inital commit, just skeleton code

---
+++ battle-srv/src/battle_srv/core.clj
@@ -0,0 +1,8 @@
+(ns battle-srv.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(defn )
---
+++ battle-srv/test/battle_srv/core_test.clj
@@ -0,0 +1,7 @@
+(ns battle-srv.core-test
+  (:require [clojure.test :refer :all]
+            [battle-srv.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
