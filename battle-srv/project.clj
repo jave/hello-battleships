043e75a22f0212a9c829b72a43564c06055cace1
Joakim Verona <joakim@verona.se>
2014-04-25 22:55:19 +0200

    web app deps

--- battle-srv/project.clj
+++ battle-srv/project.clj
@@ -1,7 +1,16 @@
 (defproject battle-srv "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "a battleships game server"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "affero gpl 3"
+            :url ""}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.match "0.2.1"]])
+                 [org.clojure/core.match "0.2.1"]
+                 [http-kit "2.1.16"]
+                 [compojure "1.1.6"]
+                 [org.clojure/tools.cli "0.2.4"]
+                 [ring/ring-devel "1.2.2"]
+                 [ring/ring-core "1.2.2"]
+                 [ring/ring-json "0.3.1"]
+                 [javax.servlet/servlet-api "2.5"]
+
+                 ])
